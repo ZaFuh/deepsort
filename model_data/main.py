cdcb8cdf7e4c22b8b273ea26f9da138927bb9f15
zf-2022 <113913728+ZaFuh@users.noreply.github.com>
2025-08-20 00:01:01 +0400

    got the video to open throguh python and resolved scilarn cython dependicies issues

---
+++ model_data/main.py
@@ -0,0 +1,16 @@
+import os
+import cv2
+
+video_path = os.path.join(".", "vid.mp4")
+
+cap = cv2.VideoCapture(video_path)
+ret, frame = cap.read()
+
+while ret:
+    cv2.imshow("frame", frame)
+    cv2.waitKey(25)
+    
+    ret, frame = cap.read()
+
+cap.release()
+cv2.destroyAllWindows()
